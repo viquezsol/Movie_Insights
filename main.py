b20268350d119a9ff7925af27a1eaa0af215f5da
viquezsol <viquezsol209@gmail.com>
2026-03-24 17:05:45 -0600

    ft: se agrega el main

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
 from src.helpers.carga_datos import CargadorDatos
 
 def main():
-    cargador = CargadorDatos("tmdb_2020_to_2025.csv")
+    cargador = CargadorDatos("data/raw/tmdb_2020_to_2025.csv")
     df = cargador.cargar()
+
     if df is not None:
         print("\nPrimeras 5 filas:")
         print(df.head())
+
         print("\nInformación de carga:")
         print(cargador.obtener_info_carga())
 
 if __name__ == "__main__":
     main()
+
+    from src.helpers.carga_datos import CargadorDatos
+    from src.eda.EDA import ProcesadorEDA
+
+
+    def main():
+        cargador = CargadorDatos("data/raw/tmdb_2020_to_2025.csv")
+        df = cargador.cargar()
+
+        eda = ProcesadorEDA(df)
+        df_limpio = eda.limpiar_datos()
+
+        eda.guardar_csv()
+
+        print("Dataset limpio guardado correctamente")
+
+
+    if __name__ == "__main__":
+        main()
